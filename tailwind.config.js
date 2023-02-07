0f2a2a6106294b13e9de34198a05a3444bd17cb9
KZDKM <tinghan.zhu@hotmail.con>
2023-02-07 23:09:58 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "sans": ["Roboto"],
+      "yt": ["Youtube"],
+      "yt-b": ["Youtube-bold"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
